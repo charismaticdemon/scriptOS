462f3ab8376c398b128a739f247bbac2c1d55b3e
Gibrid Deda <sanina228user@gmail.com>
2019-12-01 13:05:41 +0300

    text editor updated to version 0.2

--- textEditor.py
+++ textEditor.py
@@ -1,17 +1,46 @@
+import core
+import pathlib
+
 class TextEditor:
+    def goToOs():
+        core.Core.startPage()
+
     def write():
-        data = input()
-        file = open('nntd.txt', 'w')
-        file.write(data)
-        print('success!')
+        while True:
+            fileName = input('Enter file name here: ')
+            data = input('Enter text here: ')
+            my_file = open(fileName, "w")
+            my_file.write(data)
+            my_file.close()
+            act = input('Exit?:(y or n or menu)')
+            if act == 'y':
+                TextEditor.goToOs()
+            elif act == 'n':
+                print('')
+            elif act == 'menu':
+                TextEditor.start()
+            else:
+                print('command not found')
+
 
     def read():
-        filename = input('Enter text name file >> ')
-        file = open(filename, 'r')
-        print(file.read())
+        while True:
+            filename = input('Enter text name file >> ')
+            file = open(filename, 'r')
+            print(file.read())
+            file.close()
+            vibor2 = input('Exit?: y or n or menu')
+            if vibor2 == 'y':
+                TextEditor.goToOs()
+            elif vibor2 == 'n':
+                print('ok')
+            elif act == 'menu':
+                TextEditor.start()
+            else:
+                print('Error keymap')
 
     def start():
-        print('Welcome to textEditor v0.1 alpha! \n')
+        print('Welcome to textEditor v0.2! \n')
         act = input('write or read >> ')
         if act == 'write':
             TextEditor.write()
@@ -19,6 +48,8 @@ class TextEditor:
             TextEditor.read()
         else:
             print('Command not found')
+
+
 def access():
     print('access error')
 
