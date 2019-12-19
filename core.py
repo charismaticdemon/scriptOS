03f53c2ff55efb878eb363ec893cc96ba61cd561
Gibrid Deda <sanina228user@gmail.com>
2019-12-19 20:38:09 +0300

    core updated to version 0.2

--- core.py
+++ core.py
@@ -1,8 +1,44 @@
 import user_data
 import os
-class Core:
-    def startPage():
-        print("""
-        1)textEditor""")
+import json
 
-    
+class Core:
+    #ВХОД В СИСТЕМУ
+    def singIn():
+        act = input('SLM menu>> ')
+        if act == 'register':
+            os.system('cls')
+            user_data.registration()
+        elif act == 'singin':
+            os.system('cls')
+            user_data.singIn()
+        elif act == 'help':
+            print("""
+            """)
+            Core.singIn()
+    #ГЛАВНАЯ СТРАНИЦА
+    def startPage():
+        while True:
+            #ОТОБРАЖЕНИЕ ПОЛЬЗОВАТЕЛЯ В ОБОЛОЧКЕ
+            with open('userdata', 'r') as file:
+                global b
+                b = json.load(file)
+            act = input(b["login"] + '@scriptOS ~]# ')
+            #ОСНОВНЫЕ КОМАНДЫ
+            if act == 'help':
+                print("""
+                    all - show all applications
+                    show - view catalog
+                    clear - clear terminal
+                    use - choose application for open file
+                    whoami - view user
+                    version - view system version""")
+            else:
+                print('unknown command')
+
+
+
+
+
+if __name__ == "__main__":
+    Core.singIn()
