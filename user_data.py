c50d4cc3dbfe814d0a7eb17ca4e633e9ab848d85
Gibrid Deda <sanina228user@gmail.com>
2019-12-19 20:39:27 +0300

    authorization model created

--- user_data.py
+++ user_data.py
@@ -1,21 +1,51 @@
 from core import Core
 import os
+import json
+import time
 
-
+#РЕГИСТРАЦИЯ НОВОГО ПОЛЬЗОВАТЕЛЯ
 def registration():
-    act = input('scriptOS registration shell>>')
-    if act == 'register':
-        global name
-        global password
-        name = input('Enter new user login>>')
-        password = input('Enter new user password>>')
-        os.system('clear')
-        Core.startPage()
-    elif act == 'login':
-        pass
-    else:
-        os.system('clear')
-        registration()
+    global login
+    global password
+    login = input('Enter new user login>> ')
+    password = input('Enter new user password>> ')
 
-def chekUserData():
-    pass
+    #СЛОВАРЬ С ДАННЫМИ ПОЛЬЗОВАТЕЛЯ
+    userData = {
+        'login' : login,
+        'password' : password
+
+    }
+    #ЗАПИСЬ ПОЛЬЗОВАТЕЛЬСКИХ ДАННЫХ В JSON ФАЙЛ
+    try:
+
+        with open('userdata', 'w') as file:
+            json.dump(userData, file)
+        os.system('cls')
+        Core.startPage()
+    except FileExistsError:
+        print('file exists')
+
+        registration()
+    #ВХОД В СУЩЕСТВУЮЩИЙ АККАУНТ
+def singIn():
+    while True:
+        try:
+            with open ('userdata', 'r') as file:
+                b = json.load(file)
+            singLogin = input('Login>> ')
+            singPassword = input('Password>> ')
+            #ПРОЦЕСС ВХОДА
+            if singLogin == b["login"]:
+                if singPassword == b["password"]:
+                    os.system('cls')
+                    Core.startPage()
+                else:
+                    print('incorrect')
+            else:
+                print('incorrect')
+        except FileNotFoundError:
+            print('data not found')
+            time.sleep(1.0)
+            os.system('cls')
+            Core.singIn()
